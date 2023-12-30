14f8bb137773114ffd7535e2bf08957a82464b9d
jawa-pride <114786249+jawa-pride@users.noreply.github.com>
2023-12-30 11:37:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    alert("Tombol diklik!");
-});
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header, nav, main, footer {
+    margin: 20px;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+}
+
+nav {
+    background-color: #f2f2f2;
+    padding: 10px;
+}
+
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav li {
+    display: inline;
+    margin-right: 10px;
+}
+
+nav a {
+    text-decoration: none;
+    color: #333;
+}
+
+main {
+    padding: 20px;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
 
 
